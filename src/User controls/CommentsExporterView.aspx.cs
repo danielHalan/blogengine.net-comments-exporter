7ccacb2a78e6e1f1458a44762b8378cfd13d124a
Daniel Halan <danielhalan@gmail.com>
2011-04-12 15:57:56 +0200

    Tab convert

--- src/User controls/CommentsExporterView.aspx.cs
+++ src/User controls/CommentsExporterView.aspx.cs
@@ -8,146 +8,146 @@ using BlogEngine.Core;
 using BlogEngine.Core.Web.Controls;
 
 namespace Halan.Extensions {
-	public partial class CommentsExporterView : System.Web.UI.Page {
+  public partial class CommentsExporterView : System.Web.UI.Page {
 
-			
-		protected void Page_Load(object sender, EventArgs e) {
+      
+    protected void Page_Load(object sender, EventArgs e) {
 
-			if( !IsPostBack ) {
-				
-				BindAuthors();
-				BindPosts();
-			}
+      if( !IsPostBack ) {
+        
+        BindAuthors();
+        BindPosts();
+      }
 
-			BindMap();
-		}
+      BindMap();
+    }
 
-		private void BindPosts() {
-			foreach( Post p in Post.Posts ) {
-				if( !p.IsDeleted && p.IsPublished && CommentsExporter.HasValidComments(p) ) {
-					ListItem li = new ListItem(
-													string.Format("{0} ({1})", CutString(p.Title, 57), p.DateCreated.ToShortDateString()),
-													p.Id.ToString(), true);
-					li.Selected = true;
+    private void BindPosts() {
+      foreach( Post p in Post.Posts ) {
+        if( !p.IsDeleted && p.IsPublished && CommentsExporter.HasValidComments(p) ) {
+          ListItem li = new ListItem(
+                          string.Format("{0} ({1})", CutString(p.Title, 57), p.DateCreated.ToShortDateString()),
+                          p.Id.ToString(), true);
+          li.Selected = true;
 
-					cblPosts.Items.Add(li);
-				}
-			}
-			
-			
+          cblPosts.Items.Add(li);
+        }
+      }
+      
+      
 
-		}
-		private void BindMap() {
-			if( AuthorMaps.Count > 0 ) {
-				lbEmpty.Visible = false;
+    }
+    private void BindMap() {
+      if( AuthorMaps.Count > 0 ) {
+        lbEmpty.Visible = false;
 
-				repMap.Visible = true;
-				repMap.DataSource = AuthorMaps;
-				repMap.DataBind();
+        repMap.Visible = true;
+        repMap.DataSource = AuthorMaps;
+        repMap.DataBind();
 
-			} else {
+      } else {
 
-				repMap.Visible = false;
-				lbEmpty.Visible = true;
-			}
+        repMap.Visible = false;
+        lbEmpty.Visible = true;
+      }
 
-		}
-		private void BindAuthors() {
+    }
+    private void BindAuthors() {
 
-			List<Comment> allComments = new List<Comment>();
-			foreach( Post p in Post.Posts )
-				CollectAllComments(p.Comments, allComments);
+      List<Comment> allComments = new List<Comment>();
+      foreach( Post p in Post.Posts )
+        CollectAllComments(p.Comments, allComments);
 
-			List<AuthorData> authors = new List<AuthorData>();
-			foreach( Comment c in allComments ) {
-				if( (c.Email != "pingback") && (c.Email != "trackback") && !Contains(authors, c) )
-					authors.Add(new AuthorData(c.Author, c.Email, c.Website));
-			}
+      List<AuthorData> authors = new List<AuthorData>();
+      foreach( Comment c in allComments ) {
+        if( (c.Email != "pingback") && (c.Email != "trackback") && !Contains(authors, c) )
+          authors.Add(new AuthorData(c.Author, c.Email, c.Website));
+      }
 
-			authors.Sort(AuthorComparer);
+      authors.Sort(AuthorComparer);
 
-			foreach( AuthorData a in authors )
-				ddlCurrentName.Items.Add(new ListItem(string.Format("{0} ({1})", a.Name, a.Email), a.ToString()));
+      foreach( AuthorData a in authors )
+        ddlCurrentName.Items.Add(new ListItem(string.Format("{0} ({1})", a.Name, a.Email), a.ToString()));
 
 
-			if( ddlCurrentName.Items.Count == 0 )
-				btnAddNameMap.Enabled = false;
-			else {
-				lbAuthorEmail.Text = !string.IsNullOrEmpty(authors[0].Email) ? authors[0].Email : "<i>No e-mail provided</i>";
-				lbAuthorUrl.Text = !string.IsNullOrEmpty(authors[0].URL) ? authors[0].URL : "<i>No web page provided</i>";
-			}
-		}
+      if( ddlCurrentName.Items.Count == 0 )
+        btnAddNameMap.Enabled = false;
+      else {
+        lbAuthorEmail.Text = !string.IsNullOrEmpty(authors[0].Email) ? authors[0].Email : "<i>No e-mail provided</i>";
+        lbAuthorUrl.Text = !string.IsNullOrEmpty(authors[0].URL) ? authors[0].URL : "<i>No web page provided</i>";
+      }
+    }
 
-		private string CutString(string str, int length) {
-			return (str != null && str.Length > length) ? str.Substring(0, length-3) + "..." : str;
-		}
-		private int AuthorComparer(AuthorData a, AuthorData b) {
-			return string.Compare(a.Name, b.Name);
-		}
-		private bool Contains(List<AuthorData> authors, Comment c) {
-			foreach( AuthorData a in authors ) {
-				if( a.Name.Equals(c.Author, StringComparison.CurrentCultureIgnoreCase) &&
-						a.Email.Equals(c.Email, StringComparison.CurrentCultureIgnoreCase) )
-					return true;
-			}
+    private string CutString(string str, int length) {
+      return (str != null && str.Length > length) ? str.Substring(0, length-3) + "..." : str;
+    }
+    private int AuthorComparer(AuthorData a, AuthorData b) {
+      return string.Compare(a.Name, b.Name);
+    }
+    private bool Contains(List<AuthorData> authors, Comment c) {
+      foreach( AuthorData a in authors ) {
+        if( a.Name.Equals(c.Author, StringComparison.CurrentCultureIgnoreCase) &&
+            a.Email.Equals(c.Email, StringComparison.CurrentCultureIgnoreCase) )
+          return true;
+      }
 
-			return false;
-		}
-		private void CollectAllComments(List<Comment> comments, List<Comment> bag) {
-			foreach( Comment c in comments ) {
-				if( c.IsApproved ) {
-					bag.Add(c);
+      return false;
+    }
+    private void CollectAllComments(List<Comment> comments, List<Comment> bag) {
+      foreach( Comment c in comments ) {
+        if( c.IsApproved ) {
+          bag.Add(c);
 
-					CollectAllComments(c.Comments, bag);
-				}
-			}
-		}
-		
-		protected void OnAddNameMapClick(object sender, EventArgs e) {
-			if( ddlCurrentName.SelectedItem != null && tbDestName.Text.Length > 0 ) {
+          CollectAllComments(c.Comments, bag);
+        }
+      }
+    }
+    
+    protected void OnAddNameMapClick(object sender, EventArgs e) {
+      if( ddlCurrentName.SelectedItem != null && tbDestName.Text.Length > 0 ) {
 
-				AuthorData data = AuthorData.Parse(ddlCurrentName.SelectedItem.Value);
-				AuthorMap map = new AuthorMap();
-				map.BlogEngineAuthor = data;
-				map.DesitnationAuthor.Name = tbDestName.Text;
-				map.DesitnationAuthor.Email = !string.IsNullOrEmpty(tbDestEmail.Text) ? tbDestEmail.Text : data.Email;
-				map.DesitnationAuthor.URL = !string.IsNullOrEmpty(tbDestUrl.Text) ? tbDestUrl.Text : data.URL;
-				AuthorMaps.Add(map);
+        AuthorData data = AuthorData.Parse(ddlCurrentName.SelectedItem.Value);
+        AuthorMap map = new AuthorMap();
+        map.BlogEngineAuthor = data;
+        map.DesitnationAuthor.Name = tbDestName.Text;
+        map.DesitnationAuthor.Email = !string.IsNullOrEmpty(tbDestEmail.Text) ? tbDestEmail.Text : data.Email;
+        map.DesitnationAuthor.URL = !string.IsNullOrEmpty(tbDestUrl.Text) ? tbDestUrl.Text : data.URL;
+        AuthorMaps.Add(map);
 
-				tbDestName.Text = string.Empty;
-				tbDestEmail.Text = string.Empty;
-				tbDestUrl.Text = string.Empty;
+        tbDestName.Text = string.Empty;
+        tbDestEmail.Text = string.Empty;
+        tbDestUrl.Text = string.Empty;
 
-				ddlCurrentName.Items.RemoveAt(ddlCurrentName.SelectedIndex);
-				if( ddlCurrentName.Items.Count == 0 )
-					btnAddNameMap.Enabled = false;
+        ddlCurrentName.Items.RemoveAt(ddlCurrentName.SelectedIndex);
+        if( ddlCurrentName.Items.Count == 0 )
+          btnAddNameMap.Enabled = false;
 
-				BindMap();
+        BindMap();
 
-			}
-		}
-		protected void OnExportCommentClick(object sender, EventArgs e) {
-			List<Guid> s = new List<Guid>();
-			foreach( ListItem li in cblPosts.Items )
-				if( li.Selected )
-					s.Add(new Guid(li.Value));
+      }
+    }
+    protected void OnExportCommentClick(object sender, EventArgs e) {
+      List<Guid> s = new List<Guid>();
+      foreach( ListItem li in cblPosts.Items )
+        if( li.Selected )
+          s.Add(new Guid(li.Value));
 
-			if( s.Count == 0 ) 
-				return;
-			
-			Response.Clear();
-			Response.AddHeader("Content-Disposition", "attachment; filename=Comments-WordPress.wxr");
-			new CommentsExporter().Export(Response.OutputStream, s, AuthorMaps);
-			Response.End();
-		}
+      if( s.Count == 0 ) 
+        return;
+      
+      Response.Clear();
+      Response.AddHeader("Content-Disposition", "attachment; filename=Comments-WordPress.wxr");
+      new CommentsExporter().Export(Response.OutputStream, s, AuthorMaps);
+      Response.End();
+    }
 
-		private List<AuthorMap> AuthorMaps {
-			get {
-				if( ViewState["authMap"] == null )
-					ViewState["authMap"] = new List<AuthorMap>();
+    private List<AuthorMap> AuthorMaps {
+      get {
+        if( ViewState["authMap"] == null )
+          ViewState["authMap"] = new List<AuthorMap>();
 
-				return (List<AuthorMap>)ViewState["authMap"];
-			}
-		}
-	}
+        return (List<AuthorMap>)ViewState["authMap"];
+      }
+    }
+  }
 }
